d8e573a70c51848f68f671e7b45e7d466434f860
Muhammad Renaldy Ramadhan <renaldyramadhan567@gmail.com>
2025-05-13 18:21:08 +0700

    feat: add websocket and huey

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
+import os
 from django.contrib import admin
 from django.urls import path, include
+from core.consumer import NotificationConsumer
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,3 +10,12 @@ urlpatterns = [
     path("", include("apps.cv.urls")),
     path("", include("apps.dashboard.urls")),
 ]
+
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi()),
+]
+
+if os.getenv("DEBUG") == "True":
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
